d85008bf7775632b91bf7428945a94543b215416
keke0813 <149664866+keke0813@users.noreply.github.com>
2023-12-10 05:13:13 +0000

    4-8

---
+++ db/migrate/20231210045344_create_postlmages.rb
@@ -0,0 +1,11 @@
+class CreatePostlmages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :Postlmages do |t|
+
+      t.string :shop_name
+      t.text :caption
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_09_044607) do
+ActiveRecord::Schema.define(version: 2023_12_10_045344) do
+
+  create_table "Postlmages", force: :cascade do |t|
+    t.string "shop_name"
+    t.text "caption"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
